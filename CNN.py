56885912ad5c98b7f9d917c6ecf9369f42a5d9ff
Howard Huang <howardhuang2004@gmail.com>
2020-05-30 18:34:18 -0700

    data preparation

--- CNN.py
+++ CNN.py
@@ -171,3 +171,291 @@ df = pd.DataFrame(m,columns=['w','h','c','max_pixel_val','min_pixel_val'])
 df_mont = pd.concat([df_mont,df],axis=1, sort=False)
 
 df_mont.head()
+
+#Channels (stacked images patches on top of each other)
+df_shen['c'].value_counts()
+df_mont['c'].value_counts()
+
+#Create Train and Validation Sets
+df_mont['target'].value_counts()
+
+### Combine the two dataframes and shuffle
+
+df_data = pd.concat([df_shen, df_mont], axis=0).reset_index(drop=True)
+df_data = shuffle(df_data)
+df_data.shape
+
+# Create a new column called 'labels' that maps the classes to binary values.
+df_data['labels'] = df_data['target'].map({'Normal':0, 'Tuberculosis':1})
+
+df_data.head()
+
+# train_test_split
+
+y = df_data['labels']
+
+df_train, df_val = train_test_split(df_data, test_size=0.15, random_state=101, stratify=y)
+
+print(df_train.shape)
+print(df_val.shape)
+
+df_train['target'].value_counts()
+df_val['target'].value_counts()
+
+#Create Directory Structure
+
+# Create a new directory
+base_dir = 'base_dir'
+os.mkdir(base_dir)
+
+
+#[CREATE FOLDERS INSIDE THE BASE DIRECTORY]
+
+# now we create 2 folders inside 'base_dir':
+
+# train
+    # Normal
+    # Tuberculosis
+
+# val
+    # Normal
+    # Tuberculosis
+
+
+# create a path to 'base_dir' to which we will join the names of the new folders
+# train_dir
+train_dir = os.path.join(base_dir, 'train_dir')
+os.mkdir(train_dir)
+
+# val_dir
+val_dir = os.path.join(base_dir, 'val_dir')
+os.mkdir(val_dir)
+
+
+# [CREATE FOLDERS INSIDE THE TRAIN AND VALIDATION FOLDERS]
+# Inside each folder we create seperate folders for each class
+
+# create new folders inside train_dir
+Normal = os.path.join(train_dir, 'Normal')
+os.mkdir(Normal)
+Tuberculosis = os.path.join(train_dir, 'Tuberculosis')
+os.mkdir(Tuberculosis)
+
+
+# create new folders inside val_dir
+Normal = os.path.join(val_dir, 'Normal')
+os.mkdir(Normal)
+Tuberculosis = os.path.join(val_dir, 'Tuberculosis')
+os.mkdir(Tuberculosis)
+
+#Transfer images into folders
+# Set the image_id as the index in df_data
+df_data.set_index('image_id', inplace=True)
+
+
+
+# Get a list of images in each of the two folders
+folder_1 = os.listdir('../input/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png')
+folder_2 = os.listdir('../input/Montgomery/MontgomerySet/CXR_png')
+
+# Get a list of train and val images
+train_list = list(df_train['image_id'])
+val_list = list(df_val['image_id'])
+
+
+
+# Transfer the train images
+
+for image in train_list:
+
+    fname = image
+    label = df_data.loc[image,'target']
+
+    if fname in folder_1:
+        # source path to image
+        src = os.path.join('../input/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png', fname)
+        # destination path to image
+        dst = os.path.join(train_dir, label, fname)
+
+        image = cv2.imread(src)
+        image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        # save the image at the destination
+        cv2.imwrite(dst, image)
+        #shutil.copyfile(src, dst)
+
+    if fname in folder_2:
+        # source path to image
+        src = os.path.join('../input/Montgomery/MontgomerySet/CXR_png', fname)
+        # destination path to image
+        dst = os.path.join(train_dir, label, fname)
+
+        image = cv2.imread(src)
+        image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        # save the image at the destination
+        cv2.imwrite(dst, image)
+
+        # copy the image from the source to the destination
+        #shutil.copyfile(src, dst)
+
+
+# Transfer the val images
+
+for image in val_list:
+
+    fname = image
+    label = df_data.loc[image,'target']
+
+    if fname in folder_1:
+        # source path to image
+        src = os.path.join('../input/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png', fname)
+        # destination path to image
+        dst = os.path.join(val_dir, label, fname)
+
+        image = cv2.imread(src)
+        image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        # save the image at the destination
+        cv2.imwrite(dst, image)
+
+        # copy the image from the source to the destination
+        #shutil.copyfile(src, dst)
+
+    if fname in folder_2:
+        # source path to image
+        src = os.path.join('../input/Montgomery/MontgomerySet/CXR_png', fname)
+        # destination path to image
+        dst = os.path.join(val_dir, label, fname)
+
+        image = cv2.imread(src)
+        image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        # save the image at the destination
+        cv2.imwrite(dst, image)
+
+        # copy the image from the source to the destination
+        #shutil.copyfile(src, dst)
+
+# check how many train images we have in each folder
+
+print(len(os.listdir('base_dir/train_dir/Normal')))
+print(len(os.listdir('base_dir/train_dir/Tuberculosis')))
+
+# check how many val images we have in each folder
+
+print(len(os.listdir('base_dir/val_dir/Normal')))
+print(len(os.listdir('base_dir/val_dir/Tuberculosis')))
+
+#Copy train images into aug_dir to temporarily store images before augmentation
+#and then feeding into the generator
+
+class_list = ['Normal','Tuberculosis']
+
+for item in class_list:
+
+    # We are creating temporary directories here because we delete these directories later.
+    # create a base dir
+    aug_dir = 'aug_dir'
+    os.mkdir(aug_dir)
+    # create a dir within the base dir to store images of the same class
+    img_dir = os.path.join(aug_dir, 'img_dir')
+    os.mkdir(img_dir)
+
+    # Choose a class
+    img_class = item
+
+    # list all images in that directory
+    img_list = os.listdir('base_dir/train_dir/' + img_class)
+
+    # Copy images from the class train dir to the img_dir e.g. class 'Normal'
+    for fname in img_list:
+            # source path to image
+            src = os.path.join('base_dir/train_dir/' + img_class, fname)
+            # destination path to image
+            dst = os.path.join(img_dir, fname)
+            # copy the image from the source to the destination
+            shutil.copyfile(src, dst)
+
+
+    # point to a dir containing the images and not to the images themselves
+    path = aug_dir
+    save_path = 'base_dir/train_dir/' + img_class
+
+    # Create a data generator
+    datagen = ImageDataGenerator(
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+    batch_size = 50
+
+    aug_datagen = datagen.flow_from_directory(path,
+                                           save_to_dir=save_path,
+                                           save_format='png',
+                                                    target_size=(IMAGE_HEIGHT,IMAGE_WIDTH),
+                                                    batch_size=batch_size)
+
+
+    # Generate the augmented images and add them to the training folders
+
+
+    num_files = len(os.listdir(img_dir))
+
+    # this creates a similar amount of images for each class
+    num_batches = int(np.ceil((NUM_AUG_IMAGES_WANTED-num_files)/batch_size))
+
+    # run the generator and create augmented images
+    for i in range(0,num_batches):
+
+        imgs, labels = next(aug_datagen)
+
+    # delete temporary directory with the raw image files
+    shutil.rmtree('aug_dir')
+
+# Check how many train images we now have in each folder.
+# This is the original images plus the augmented images.
+
+print(len(os.listdir('base_dir/train_dir/Normal')))
+print(len(os.listdir('base_dir/train_dir/Tuberculosis')))
+
+# Check how many val images we have in each folder.
+
+print(len(os.listdir('base_dir/val_dir/Normal')))
+print(len(os.listdir('base_dir/val_dir/Tuberculosis')))
+
+# plots images with labels within jupyter notebook
+# source: https://github.com/smileservices/keras_utils/blob/master/utils.py
+
+def plots(ims, figsize=(20,10), rows=5, interp=False, titles=None): # 12,6
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
+
+plots(imgs, titles=None) # titles=labels will display the image labels
+
+# End of Data Preparation
+### ===================================================================================== ###
+# Start of Model Building
+
+#Set Up Generators
+
+train_path = 'base_dir/train_dir'
+valid_path = 'base_dir/val_dir'
+
+num_train_samples = len(df_train)
+num_val_samples = len(df_val)
+train_batch_size = 10
+val_batch_size = 10
+
+
+train_steps = np.ceil(num_train_samples / train_batch_size)
+val_steps = np.ceil(num_val_samples / val_batch_size)
